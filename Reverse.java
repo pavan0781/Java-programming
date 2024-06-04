e533a9ca434b4d6d3ba2847ecd0190095d4dc361
MrPavan <113451785+pavan0781@users.noreply.github.com>
2024-06-04 08:49:53 +0530

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,18 @@
+Reverse a Number                                                                                                                                                                                 import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int number = scanner.nextInt();
+        int reverse = 0;
+
+        while (number != 0) {
+            int digit = number % 10;
+            reverse = reverse * 10 + digit;
+            number /= 10;
+        }
+
+        System.out.println("Reverse Number: " + reverse);
+    }
+}
