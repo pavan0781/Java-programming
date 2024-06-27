c24d90b7c77f9e24b1d02de8bfa0f65bcb90c1f7
MrPavan <113451785+pavan0781@users.noreply.github.com>
2024-06-27 10:02:12 +0530

    Add files via upload

---
+++ ASSIGNMENT 2/2)Rectangle Area.java
@@ -0,0 +1,14 @@
+public class rectangle{  
+    public static void main(String args[])  
+    {  
+    int width=5;  
+    int height=10;  
+    int area=width*height;  
+        System.out.println("Area of rectangle="+area);
+}
+}
+OUTPUT
+java -cp /tmp/CiwgQePNPN/rectangle
+Area of rectangle=50
+
+=== Code Execution Successful ===
